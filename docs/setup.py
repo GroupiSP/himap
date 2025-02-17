678535c31f60bec32e7170dc72d7ac0b98537954
Mariana Salinas Camus <150278041+mariana-sc@users.noreply.github.com>
2025-02-17 12:05:25 +0100

    Update setup.py

--- docs/setup.py
+++ docs/setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-import glob
 
 setup(
-    ext_modules=cythonize(glob.glob("himap/cython_build/fwd_bwd.pyx"),compiler_directives={'boundscheck': False})
+    setup_requires=['setuptools>=18.0', 'cython'],
+    ext_modules=cythonize([
+        Extension(
+            'himap.cython_build.fwd_bwd',
+            sources=['himap/cython_build/fwd_bwd.pyx'],
+        ),
+    ], compiler_directives={'boundscheck': False}),
 )
